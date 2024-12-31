f407055077eb2e54b38c5c3dc473482534b2c175
bourassine <bourassineimen5@gmail.com>
2024-12-31 09:34:27 +0100

    Correction CRUD (imen)

--- src/Form/DoctorType.php
+++ src/Form/DoctorType.php
@@ -2,23 +2,36 @@
 
 namespace App\Form;
 
+use App\Entity\Doctor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class DoctorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
-        ;
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('speciality', TextType::class, [
+                'label' => 'Spécialité',
+                'required' => true,
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => Doctor::class,
         ]);
     }
 }
